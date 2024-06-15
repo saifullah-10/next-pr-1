f27e3f83b8cb82d12830105a2ee8fa78cfbe799a
saifullah-10 <saifullah.bus@gmail.com>
2024-06-15 20:34:26 +0600

    initializing project

---
+++ components/Hero.tsx
@@ -0,0 +1,30 @@
+import { Spotlight } from "./ui/Spotlight";
+
+export default function Hero() {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        ></Spotlight>
+        <Spotlight
+          className="top-10 left-full  h-[80vh] w-[50vw] "
+          fill="purple"
+        ></Spotlight>
+        <Spotlight
+          className="top-28 left-80  h-[80vh] w-[50vw]"
+          fill="blue"
+        ></Spotlight>
+      </div>
+
+      <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+          Backgrounds
+        </p>
+      </div>
+    </div>
+  );
+}
